1d5019f29eb2f6a3936dfd7127255bf5ce5c0a8e
bodhili <ybodhili@gmail.com>
2024-09-30 16:58:46 +0800

    Add Javadoc comments to ValueWrapper class
    Enhanced the ValueWrapper class with detailed Javadoc comments for all attributes. These comments provide clear explanations of the purpose and usage of each field, improving code readability and maintainability.

--- src/main/java/org/venus/cache/ValueWrapper.java
+++ src/main/java/org/venus/cache/ValueWrapper.java
@@ -4,6 +4,15 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+/**
+ * ValueWrapper represents the essential data for URL mapping and redirection.
+ *
+ * This class holds the attributes needed to map a short code to the original URL
+ * and manage redirection behavior, including tracking the expiry and activation status.
+ *
+ * It is typically used within URL shortening services or systems handling URL redirection
+ * and mapping.
+ */
 @Getter
 @Setter
 @Builder
@@ -12,10 +21,48 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @ToString
 public class ValueWrapper {
+    /**
+     * Unique identifier for the ValueWrapper instance.
+     *
+     * This ID serves as a primary key used to uniquely distinguish each instance
+     * of ValueWrapper in storage and other operations.
+     */
     private long id;
+    /**
+     * Represents a unique short code associated with a URL.
+     *
+     * This field holds the shortened code that maps to the original URL
+     * for redirection purposes in a URL shortening service.
+     */
     private String code;
+    /**
+     * Represents the number of times a URL redirection has occurred.
+     *
+     * This variable is used within the ValueWrapper class to track how many times
+     * the associated short code has redirected users to the original URL.
+     */
     private int redirect;
+    /**
+     * The original URL that corresponds to a shortened URL code.
+     *
+     * This attribute holds the complete URL before it was shortened. It is used
+     * to redirect the user to the intended web page when the shortened URL code
+     * is accessed.
+     */
     private String originalUrl;
+    /**
+     * The date and time when this URL mapping expires.
+     *
+     * Indicates when the current URL mapping should be considered inactive and not valid for redirection.
+     * This is typically used to manage the lifecycle of shortened URLs, ensuring they are only valid
+     * for a certain period.
+     */
     private LocalDateTime expiresAt;
+    /**
+     * Indicates whether the URL mapping is currently active.
+     *
+     * A value of 1 denotes that the URL mapping is active and can be used for redirection,
+     * while a value of 0 indicates that the URL mapping has been deactivated or is not in use.
+     */
     private short isActive;
 }
